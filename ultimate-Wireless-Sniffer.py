a90c3431e67fbcbe1dca22d04b54e019cee9b2df
Phystro <35362037+Phystro@users.noreply.github.com>
2018-11-23 12:50:50 +0300

    Add files via upload

--- ultimate-Wireless-Sniffer.py
+++ ultimate-Wireless-Sniffer.py
@@ -5,27 +5,43 @@ import optparse, os
 hidden = []
 unhidden = []
 
+#Showing time
 def tt():
     t = datetime.now()
     h,m,s = t.hour, t.minute, t.second
     h,m,s = str(h), str(m), str(s)
     tt = h+"."+m+"."+s
     return tt
+#File for captured devices info
 def addf(fname, mac, ch):
-    f = open("stashbox","a")
     if ch == None:
         ch = None
-        device = "[DE]"
+        device = "[UsersDevice]"
     elif ch != None:
-        device = "[AP]"
-    content = "%s ESSID: %s    MAC: %s     Channel: %s\n"%(device,fname,mac,ch)
-    f.write(content)
+        device = "[AccessPoint]"
+    content = "%s MAC: %s   Channel: %s     ESSID: %s\n"%(device,mac,ch,fname)
+    f = open("stashbox","r")
+    if content in f:
+        pass
+    elif content not in f:
+        fl = open("stashbox","a")
+        fl.write(content)
+        fl.close()
     f.close()
+#File for logs
 def addlog(logs):
-    f = open("sniffer.log","a")
     content = logs+"\n"
-    f.write(content)
+    f = open("sniffer.log","a")
     f.close()
+    f = open("sniffer.log","r")
+    if content in f:
+        pass
+    elif content not in f:
+        fl = open("sniffer.log","a")
+        fl.write(content)
+        fl.close()
+    f.close()
+#Decloaking hidden networks
 def decloak(p):
     macaddr = p.getlayer(Dot11).addr2
     channel = ord(pkt[Dot11Elt:3].info)
@@ -62,35 +78,38 @@ def decloak(p):
             addlog(log)
             unhidden.append(macaddr)
             addf(netname,macaddr,None)
+#Beacon Frames
 def beaconframe(pkt):
     ssid = pkt.getlayer(Dot11Beacon).info
     macaddr = pkt.getlayer(Dot11).addr2
     channel = ord(pkt[Dot11Elt:3].info)
     s = tt()
-    log = ("%s [+] Detected Beacon Frame: %s     MAC: %s    Channel: %s"%(s,ssid,macaddr,channel))
+    log = ("%s [+] Detected Beacon Frame:   from MAC: %s  on Channel: %s\tAs ESSID: %s"%(s,macaddr,channel,ssid))
     print log
     addlog(log)
     if macaddr not in unhidden:
         unhidden.append(macaddr)
         addf(ssid,macaddr,channel)
+#Probe Requests
 def preq(pkt):
     name = pkt.getlayer(Dot11ProbeReq).info
     mac2 = pkt.getlayer(Dot11).addr2
     s = tt()
     if len(name) == 0:
         name = "unknown"
-    log = ("%s [+] Detected Probe Request: %s    MAC: %s"%(s,name,mac2))
+    log = ("%s [+] Detected Probe Request:  from MAC: %s\t\t\tTo ESSID: %s"%(s,mac2,name))
     print log
     addlog(log)
     if mac2 not in unhidden:
         unhidden.append(mac2)
         addf(name,mac2,None)
+#Probe Responses
 def presp(pkt):
     ssid = pkt.getlayer(Dot11ProbeResp).info
     mac2 = pkt.getlayer(Dot11).addr2
     mac1 = pkt.getlayer(Dot11).addr1
     s = tt()
-    log = ("%s [+] Detected Probe Response: %s   MAC: %s     Client: %s"%(s,ssid,mac2,mac1))
+    log = ("%s [+] Detected Probe Response: from MAC: %s\t\t\tTo Client MAC: %s\tAs ESSID: %s"%(s,mac2,mac1,ssid))
     print log
     addlog(log)
     if mac2 not in unhidden:
@@ -105,8 +124,37 @@ def main(p):
     #probes, Probe Requests and Probe Responses
     elif p.haslayer(Dot11ProbeReq):
         preq(p)
-    if p.haslayer(Dot11ProbeResp):
+    elif p.haslayer(Dot11ProbeResp):
         presp(p)
+    #Others to work on
+    elif p.haslayer(Dot11AssoReq):
+        print("[+] Detected Association request")
+    elif p.haslayer(Dot11AssoResp):
+        print("[+] Detected Association Response")
+    elif p.haslayer(Dot11Disas):
+        print("[+] Detected Disassociation")
+    elif p.haslayer(Dot11Auth):
+        print("[+] Detected Authentication")
+    elif p.haslayer(Dot11Deauth):
+        print("[+] Detected Deauthentication")
+    elif p.haslayer(Dot11ProbeReq):
+        print("[+] Detected Probe request")
+    elif p.haslayer(Dot11ProbeResp):
+        print("[+] Detected Probe Response")
+    elif p.haslayer(Dot11Elt):
+        print("[+] Detected 802.11 Rates INformation ELement")
+    elif p.haslayer(Dot11QoS):
+        print("[+] Detected QoS")
+    elif p.haslayer(Dot11ReassoReq):
+        print("[+] Detected Reassociation Request")
+    elif p.haslayer("[+] Detected ReassoResp"):
+        print("[+] Detected Reassociation Response")
+    elif p.haslayer(Dot11WEP):
+        print("[+] Detcted WEP packet")
+    elif p.haslayer(TCP):
+        print("[+] Detected a TCP Packet")
+    elif p.haslayer(DNS):
+        print("[+] Detected a DNS Packet")
     
 if __name__=="__main__":
     parser = optparse.OptionParser("usage: python2 %proc [option]\
@@ -127,12 +175,13 @@ if __name__=="__main__":
     date = "*"*38+" Date: "+d+"/"+m+"/"+y+"/"+"_"*13+"Start Time: "+s+" "+"*"*38
     print date,"\n"
     addlog(date)
-    
+    #print "\n Time\t\tDetection Type\t\tAP ESSID\t\tClient MAC\t\t\tAP MAC"
     if (options.interface != None):
         conf.iface = options.interface
         sniff(prn=main, store=0)
     elif (options.interface == None):
-        interface = raw_input("Interface to use: ")
+        interface = raw_input("\nInterface to use: ")
+        print "\n"
         conf.iface = interface
         sniff(prn=main)
     else:
